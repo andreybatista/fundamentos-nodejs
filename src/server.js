7f2545832b7eef1ae7cd52a881aae437f5c7b132
andreybatista <andreybatista131@gmail.com>
2023-04-04 16:18:31 -0300

    banco de dados em json e rotas em array

--- src/server.js
+++ src/server.js
@@ -1,5 +1,9 @@
 import http from "node:http";
 
+import { json } from "./middlewares/json.js";
+import { Database } from "./database.js";
+import { routes } from "./routes.js";
+
 // GET, POST, PUT, PATCH, DELETE
 
 // GET => Buscar um recurso do back-end
@@ -12,44 +16,19 @@ import http from "node:http";
 
 // HTTP Status Code
 
-const users = []
 const server = http.createServer(async (req, res) => {
 
   const { method, url } = req
 
+  await json(req, res)
 
-  const buffers = []
+  const route = routes.find(route => {
+    return route.method === method && route.path === url
+  })
 
-  for await (const chunk of req) {
-    buffers.push(chunk)
+  if(route) {
+    return route.handler(req, res)
   }
-
-  try {
-    req.body = JSON.parse(Buffer.concat(buffers).toString())
-  } catch {
-    req.body = null
-  }
-
-
-  if (method === 'GET' && url === '/users') {
-    return res
-      .setHeader('Content-type', 'application/json')
-      .end(JSON.stringify(users))
-  }
-  if (method === 'POST' && url === '/users') {
-
-    const { name, email } = req.body
-
-    users.push({
-      id: 1,
-      name,
-      email
-    })
-    return res
-      .writeHead(201)
-      .end()
-  }
-
 
   return res.writeHead(404).end()
 });
